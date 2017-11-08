47570061a11638706073d22884a0eff36ade51dd
Matthew Bauer <bauer.s.matthew@gmail.com>
2017-11-08 13:56:31 -0800

    Make it possible to give multi-line inputs.
    Modify help message accordingly.
    Make 'cancel' on its own line not cancel anymore (only 'CANCEL' works
    now).
    Remove random crap at the top of the file.

--- hypernote/input/editor.py
+++ hypernote/input/editor.py
@@ -4,20 +4,18 @@ import os
 import subprocess
 from hypernote.input.base import Cancelled, Invalid
 
-class Cancelled(RuntimeError):
-    pass
-class Invalid(RuntimeError):
-    pass
-
 def input_note(note_cls, prefilled={}, editor='vim'):
     """Get note details from user using a text editor.
 
     note_cls: the class of the note to get.
     editor: the name (str) of the editor to use."""
     text = ('# Fill in details about the note below.\n'
-            '# Lines beginning with \'#\' are comments.\n'
             '# To cancel, write \'CANCEL\' on its own line, anywhere in the'
             ' file.\n'
+            '# Lines beginning with \'#\' are comments.\n'
+            '# Lines beginning with \'$\' mark the beginning of a field;'
+            ' the field includes all the text after the \'=\' and before the'
+            ' next field or the end of the file.\n'
             '# If a field is left blank, HyperNote will try to autofill it.'
             '\n\n')
     dispname_to_name = {}
@@ -32,7 +30,7 @@ def input_note(note_cls, prefilled={}, editor='vim'):
             text += '# REQUIRED\n'
         if part.name in note_cls.unsafe:
             text += "# MAY BE UNSAFE TO AUTOFILL; TYPE '@@' TO AUTOFILL.\n"
-        text += disp_name + ' = '
+        text += '$ {} = '.format(disp_name)
         if part.name in prefilled:
             text += prefilled[part.name]
         text += '\n\n'
@@ -45,23 +43,39 @@ def input_note(note_cls, prefilled={}, editor='vim'):
     editor_process.wait()
 
     # read the file
+    inp_text = None
     with open(filename) as fin:
         inp_text = list(fin.readlines())
     info = {}
+    cur_field = None
+    cur_field_lines = []
     for line in inp_text:
+        app_text = ''
         line = line.strip()
-        if not line or line[0] == '#':
-            # comment or empty
-            continue
-        if line in ('CANCEL', 'cancel'):
-            # cancelled
+        if line == 'CANCEL':
+            # CANCEL on cancel lines
             raise Cancelled()
-        if line.count('=') != 1:
-            # not a valid input line
-            print("LINE: '{}'".format(line))
-            raise Invalid()
-        key, val = line.split('=')
-        key, val = key.strip(), val.strip()
-        info[dispname_to_name[key]] = val
-
+        elif line and line[0] == '#':
+            # IGNORE comment lines
+            continue
+        elif line and line[0] == '$' and line.count('=') >= 1:
+            # STORE on field-start lines
+            if cur_field is not None:
+                # remove blank lines before and after (but not in the middle)
+                info[cur_field] = '\n'.join(cur_field_lines).strip('\n')
+            # extract new cur_field
+            new_cf_dispname = line[1:line.find('=')].strip()
+            if new_cf_dispname not in dispname_to_name:
+                raise Invalid()
+            cur_field = dispname_to_name[new_cf_dispname]
+            # extract first line
+            cur_field_lines = [line[line.find('=')+1:].strip()]
+        else:
+            # KEEP all other lines (including blank lines!)
+            cur_field_lines.append(line)
+    # STORE final field
+    if cur_field is not None:
+        # remove blank lines before and after (but not in the middle)
+        info[cur_field] = '\n'.join(cur_field_lines).strip('\n')
+               
     return info
